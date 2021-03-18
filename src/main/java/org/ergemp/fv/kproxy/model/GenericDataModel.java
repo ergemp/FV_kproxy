d184a149fcf9fe1d8cf4e2c5448dcd4c025dfdb2
ergemp <ergemp@gmail.com>
2021-03-18 12:19:25 +0300

    .

--- src/main/java/org/ergemp/fv/kproxy/model/GenericDataModel.java
+++ src/main/java/org/ergemp/fv/kproxy/model/GenericDataModel.java
@@ -1,4 +1,47 @@
 package org.ergemp.fv.kproxy.model;
 
 public class GenericDataModel {
+    private String token;
+    private Long ts;
+    private String label;
+    private String payload;
+
+    public GenericDataModel(String gToken, Long gTs, String gLabel, String gPayload){
+        this.token = gToken;
+        this.ts = gTs;
+        this.label = gLabel;
+        this.payload = gPayload;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getTs() {
+        return ts;
+    }
+
+    public void setTs(Long ts) {
+        this.ts = ts;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
 }
